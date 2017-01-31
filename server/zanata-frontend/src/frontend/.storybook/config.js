37516abc0293076d94ef954e9dc1577d7c19c79a
kgough <kgough@redhat.com>
2017-01-31 11:01:05 +1000

    Added webpack settings to storybook and fixed linting errors in example files

--- server/zanata-frontend/src/frontend/.storybook/config.js
+++ server/zanata-frontend/src/frontend/.storybook/config.js
@@ -1,16 +1,7 @@
-import { configure } from '@kadira/storybook'
-import './storybook.css'
+import { configure } from '@kadira/storybook';
 
-// fonts are included in index.html for the app, but storybook does not use that
-var fontLink = document.createElement('link')
-fontLink.setAttribute('href', 'http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,400italic') // eslint-disable-line max-len
-fontLink.setAttribute('rel', 'stylesheet')
-fontLink.setAttribute('type', 'text/css')
-fontLink.setAttribute('async', '')
-document.getElementsByTagName('head')[0].appendChild(fontLink)
-
-function loadStories () {
-  require('../app/components/stories.js')
+function loadStories() {
+  require('../stories');
 }
 
-configure(loadStories, module)
+configure(loadStories, module);
--- server/zanata-frontend/src/frontend/.storybook/webpack.config.js
+++ server/zanata-frontend/src/frontend/.storybook/webpack.config.js
@@ -1,74 +1,68 @@
-var autoprefixer = require('autoprefixer')
-var reworkCalc = require('rework-calc')
-var reworkColorFunction = require('rework-color-function')
-var reworkCustomMedia = require('rework-custom-media')
-var reworkIeLimits = require('rework-ie-limits')
-var reworkNpm = require('rework-npm')
-var reworkVars = require('rework-vars')
-var reworkSuitConformance = require('rework-suit-conformance')
-
-/*
- * This is a slightly modified version of the webpack config for the main app.
- * It is used primarily to build the css in the same way as the main app, but
- * inserts it onto the page rather than building it to a separate file.
+/**
+ * This is the base config for all builds.
  *
- * The css file is imported in .storybook/config.js to trigger the css build
- * with this configuration.
+ * Cleanup needed:
+ *  - This config is used for a first-pass build that is required for atomic css
+ *    to work. This should be fixed so that atomic works without the extra build.
+ *  - This outputs to a different file than the production build, but we could
+ *    just use the same filename for all builds.
  */
+
+var webpack = require('webpack')
+var path = require('path')
+
 module.exports = {
+  entry: './app/index',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
   module: {
-    loaders: [
-      /* Allows use of newer javascript syntax.
-       *  - mainly ES6/ES2015 syntax, and a few ES2016 things
-       *  - configured in .babelrc
-       */
+    preLoaders: [
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        loader: 'babel'
+        loader: 'eslint'
+      }
+    ],
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        include: path.join(__dirname, 'app'),
+        loader: 'atomic-loader?configPath=' + __dirname +
+        '/atomicCssConfig.js' +
+        '!babel?presets[]=react,presets[]=es2015,presets[]=stage-0'
       },
-
-      /* Bundles all the css and allows use of various niceities, including
-       * imports, variables, calculations, and non-prefixed codes.
-       */
       {
         test: /\.css$/,
+        include: [
+          path.join(__dirname, 'app/styles'),
+          path.join(__dirname, 'node_modules/zanata-ui/lib/styles'),
+        ],
+        loader: 'style!css!autoprefixer?browsers=last 2 versions'
+      },
+      {
+        test: /\.less$/,
         exclude: /node_modules/,
-        loader: 'style!css!csso!postcss!rework'
-      }
+        include: path.join(__dirname, 'app/styles'),
+        loader: "style!css!autoprefixer!less"
+      },
     ]
   },
-
-  resolve: {
-    /* Subdirectories to check while searching up tree for module */
-    // TODO remove when components migrated to use .js (default is ['', '.js'])
-    extensions: ['', '.js', '.jsx']
-  },
-
-  /* Used just to run autoprefix */
-  postcss: [
-    autoprefixer({
-      browsers: [
-        'Explorer >= 9',
-        'last 2 Chrome versions',
-        'last 2 Firefox versions',
-        'last 2 Safari versions',
-        'last 2 iOS versions',
-        'Android 4'
-      ]
-    })
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.NoErrorsPlugin()
   ],
-
-  /* Enables a range of syntax improvements and checks for css files */
-  rework: {
-    use: [
-      reworkNpm(),
-      reworkVars(),
-      reworkCalc,
-      reworkColorFunction,
-      reworkCustomMedia,
-      reworkIeLimits,
-      reworkSuitConformance
-    ]
+  resolve: {
+    extensions: ['', '.js', '.jsx', '.json', '.css', '.less']
+  },
+  node: {
+    __dirname: true
+  },
+  eslint: {
+    failOnWarning: false,
+    failOnError: true
   }
 }
