780fffa86d5c3d73ce67761361297cf0a7154cdf
David Mason <drdmason@gmail.com>
2017-03-07 10:06:40 +1000

    chore(ZNTA-1531): make sure editor properly loads config from HTML page

--- server/zanata-frontend/src/frontend/app/editor/api/index.js
+++ server/zanata-frontend/src/frontend/app/editor/api/index.js
@@ -14,7 +14,7 @@ import {
   STATUS_APPROVED
 } from '../../utils/status'
 
-const config = window.config.dataset
+const config = window.config
 
 /* The part of the path that is just the server deployment path. e.g. if the
  * server is deployed at example.com/zanata then this will be /zanata */
